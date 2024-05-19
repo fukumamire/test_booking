18842fd12a2bb1f988282548adf9f443ba8b9ee3
fukumamire <rinka_karyou@hotmail.com>
2024-05-19 19:29:38 +0900

    register(会員登録）のアイコン、テキスト枠を下線のみに変更　

--- src/app/Actions/Fortify/CreateNewUser.php
+++ src/app/Actions/Fortify/CreateNewUser.php
@@ -20,7 +20,7 @@ class CreateNewUser implements CreatesNewUsers
     public function create(array $input): User
     {
         Validator::make($input, [
-            'name' => ['required', 'string', 'regex:/^[^ ]+ [^ ]+(?: [^ ]+)?$/'],
+            'name' => ['required', 'string','max:191', 'regex:/^[^ ]+ [^ ]+(?: [^ ]+)?$/'],
             'email' => [
                 'required',
                 'string',
--- src/resources/views/auth/register.blade.php
+++ src/resources/views/auth/register.blade.php
@@ -2,6 +2,7 @@
 
 @section('css')
 <link rel="stylesheet" href="{{ asset('css/register.css') }}">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.14.0/css/all.min.css" integrity="sha512-1PKOgIY59xJ8Co8+NE6FZ+LOAZKjy+KY8iq0G4B3CyeY6wYHN3yt9PW0XpSriVlkMXe40PTKnXrLnZ9+fkDaog==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 @endsection
 
 @section('content')
@@ -13,19 +14,21 @@
     <form action="/register" method="post" class="registration-form">
       @csrf
       <div class="form-group">
-        
+        <i class="fas fa-user-alt fa-2x"></i>
         <input type="text" id="username" name="name" placeholder="username" value="{{ old('name') }}">
         @error('name')
           <div class="error-message">{{ $message }}</div>
         @enderror
       </div>
       <div class="form-group">
+        <i class="fas fa-envelope fa-2x"></i>
         <input type="email" id="email" name="email" placeholder="email" value="{{ old('email') }}">
         @error('email')
           <div class="error-message">{{ $message }}</div>
         @enderror
       </div>
       <div class="form-group">
+        <i class="fas fa-lock fa-2x"></i>
         <input type="password" id="password" name="password" placeholder="password">
         @error('password')
           <div class="error-message">{{ $message }}</div>
