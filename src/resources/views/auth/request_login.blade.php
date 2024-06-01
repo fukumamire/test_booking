3aae1e7f5e69be056d797924f2323cf4a014893c
fukumamire <rinka_karyou@hotmail.com>
2024-06-01 11:16:10 +0900

    request_login　作成途中２　新規登録用　ログイン用

--- src/resources/views/auth/request_login.blade.php
+++ src/resources/views/auth/request_login.blade.php
@@ -1,12 +1,21 @@
 @extends('layouts.app')
 
 @section('css')
-    <link rel="stylesheet" href="{{ asset('css/request_login') }}">
+    <link rel="stylesheet" href="{{ asset('css/request_login.css') }}">
 @endsection
 
 
 @section('content')
+  <div class="box effect1">
+    アカウントをお持ちでない方
+    <h3>Registration</h3>
+  </div>
 
+
+<div class="box effect1">
+    アカウントをお持ちの方
+    <h3>Login</h3>
+  </div>
 
 
 @endsection
