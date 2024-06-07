b25847750260fe1d8d0963924e62008400bc7db1
fukumamire <rinka_karyou@hotmail.com>
2024-06-08 08:25:55 +0900

    app.blade.php メニューバー　Homeの修正

--- src/database/migrations/2024_06_06_194733_create_shop_images_table.php
+++ src/database/migrations/2024_06_06_194733_create_shop_images_table.php
@@ -6,26 +6,32 @@ use Illuminate\Support\Facades\Schema;
 
 class CreateShopImagesTable extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
-    {
-        Schema::create('shop_images', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
-        });
-    }
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('shop_images', function (Blueprint $table) {
+			$table->id();
+			$table->unsignedBigInteger('shop_id');
+			$table->string('shop_image_url');
+			$table->timestamp('created_at')->useCurrent();
+			$table->timestamp('updated_at')->useCurrent()->useCurrentOnUpdate();
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
-    {
-        Schema::dropIfExists('shop_images');
-    }
+			// 外部キー制約を追加
+			$table->foreign('shop_id')->references('id')->on('shops')->onDelete('cascade');
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::dropIfExists('shop_images');
+	}
 }
