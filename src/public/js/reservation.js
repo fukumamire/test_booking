e3623c5b8b6a630fe3ed32a03c0dcc7682148b7c
fukumamire <rinka_karyou@hotmail.com>
2024-06-19 22:11:25 +0900

    便宜　詳細と予約画面の作成ログインしていないユーザーでも予約フォームを表示させるために@authディレクティブを使用せずに作成　

--- src/public/js/reservation.js
+++ src/public/js/reservation.js
@@ -1,17 +1,29 @@
-document.addEventListener('DOMContentLoaded', function () {
-  var dateInput = document.getElementById('datePicker');
-  var today = new Date().toISOString().split('T')[0];
-  dateInput.setAttribute('min', today);
+document.addEventListener("DOMContentLoaded", function () {
+  const form = document.getElementById("reservationForm");
+  if (form.classList.contains("not-authenticated")) {
+    form.addEventListener("submit", function (event) {
+      event.preventDefault();
+      window.location.href = "{{ route('request_login') }}";
+    });
+  }
 
-  dateInput.addEventListener('change', function () {
-    document.getElementById('dateSummary').textContent = this.value;
+  var dateInput = document.getElementById("datePicker");
+  var today = new Date().toISOString().split("T")[0];
+  dateInput.setAttribute("min", today);
+
+  dateInput.addEventListener("change", function () {
+    document.getElementById("dateSummary").textContent = this.value
   });
 
-  document.querySelector('select[name="time"]').addEventListener('change', function () {
-    document.getElementById('timeSummary').textContent = this.value;
-  });
+  document
+    .querySelector('select[name="time"]')
+    .addEventListener("change", function () {
+      document.getElementById("timeSummary").textContent = this.value;
+    });
 
-  document.querySelector('select[name="number"]').addEventListener('change', function () {
-    document.getElementById('numberSummary').textContent = this.value;
-  });
+  document
+    .querySelector('select[name="number"]')
+    .addEventListener("change", function () {
+      document.getElementById("numberSummary").textContent = this.value;
+    });
 });
