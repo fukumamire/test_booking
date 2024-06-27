1bf48bc97a57ba55da18388626427112477bcc11
fukumamire <rinka_karyou@hotmail.com>
2024-06-27 22:02:36 +0900

    予約状況欄　予約１　予約２　とタイトルの設定

--- src/resources/views/partials/bookings.blade.php
+++ src/resources/views/partials/bookings.blade.php
@@ -1,15 +1,11 @@
 <div class="booking__content {{ $loop->iteration % 2 == 0 ? 'booking__content--steelblue' : '' }}">
   <div class="booking__header">
-    <a href="{{ route('bookings.show', ['booking' => $booking->id]) }}" class="booking__header__title">
-      <span class="header__title">Shop{{ $booking->shop->name }}</span>
+    <span class="header__title">
+      <img src="{{ asset('images/clock.png') }}" alt="時計" class="clock-icon white-image"> 予約{{ $loop->iteration }}
+      </span>
+      <a href="{{ route('bookings.show', ['booking' => $booking->id]) }}" class="booking__header__title">
     </a>
     <div class="booking__header-button">
-      <form method="POST" action="{{ route('bookings.edit', ['booking' => $booking->id]) }}" class="header__form">
-        @csrf
-        <button type="submit" class="form__button form__button--edit">
-          <img src="{{ asset('images/pencil.png') }}" alt="編集" class="form__button-img white-image">
-        </button>
-      </form>
       <form method="POST" action="{{ route('bookings.cancel', ['booking' => $booking->id]) }}" class="header__form">
         @csrf
         <button type="submit" class="form__button form__button--cancel">
@@ -19,15 +15,15 @@
     </div>
   </div>
   <div class="booking__details">
-    <p>Date: {{ $booking->date }} </p>
-    <p>Time:
+    <p>Shop {{ $booking->shop->name }}</p>
+    <p>Date {{ $booking->date }} </p>
+    <p>Time
       @if($booking->time instanceof \DateTime)
         {{ $booking->time->format('H:i') }}
       @else
         {{ date('H:i', strtotime($booking->time)) }}
       @endif
     </p>
-    <p>Number: {{ $booking->number_of_people }}人</p>
-    {{-- <p>Status: {{ $booking->status }}</p> --}}
+    <p>Number {{ $booking->number_of_people }}人</p>
   </div>
 </div>
