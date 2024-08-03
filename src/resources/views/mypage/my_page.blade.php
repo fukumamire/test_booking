24668cced458fa6c2b0f36b68c67e39e200b80fc
fukumamire <rinka_karyou@hotmail.com>
2024-08-03 09:17:39 +0900

    マイページ　予約変更ボタンの設置　

--- src/resources/views/mypage/my_page.blade.php
+++ src/resources/views/mypage/my_page.blade.php
@@ -24,62 +24,62 @@
 
   <div class="mypage__wrap">
     <div class="left__side">
-        <div class="booking__wrap">
-            <div class="booking__tab">
-                <label class="booking__title hover__color--blue">
-                    <input type="radio" name="tab" class="booking__title-input" checked>
-                    予約状況
-                </label>
-                <div class="booking__content-wrap">
-                    @if (isset($bookings) && count($bookings) > 0)
-                        @foreach ($bookings as $booking)
-                            @include('partials.bookings', ['booking' => $booking, 'loopIteration' => $loop->iteration])
-                        @endforeach
-                    @else
-                        <div class="booking__placeholder">予約はありません</div>
-                    @endif
-                </div>
-            </div>
+      <div class="booking__wrap">
+        <div class="booking__tab">
+          <label class="booking__title hover__color--blue">
+            <input type="radio" name="tab" class="booking__title-input" checked>
+            予約状況
+          </label>
+          <div class="booking__content-wrap">
+            @if (isset($bookings) && count($bookings) > 0)
+            @foreach ($bookings as $booking)
+            @include('partials.bookings', ['booking' => $booking, 'loopIteration' => $loop->iteration])
+            @endforeach
+            @else
+            <div class="booking__placeholder">予約はありません</div>
+            @endif
+          </div>
         </div>
+      </div>
     </div>
 
     <div class="right__side">
-        <p class="user__name">{{ Auth::user()->name }}さん</p>
-        <p class="favorite__title">お気に入り店舗</p>
-        <div class="shop__wrap">
-            @if (isset($favoriteShops) && count($favoriteShops) > 0)
-                @foreach ($favoriteShops as $favorite)
-                    <div class="shop__content">
-                        @foreach ($favorite->shop->images as $image)
-                            <img src="{{ $image->shop_image_url }}" alt="{{ $favorite->shop->name }}" class="shop__image">
-                        @endforeach
-                        <div class="shop__item">
-                            <h2 class="shop__title">{{ $favorite->shop->name }}</h2>
-                            <div class="shop__tag">
-                                @foreach ($favorite->shop->areas as $area)
-                                    <p class="shop__tag-info">#{{ $area->name }}</p>
-                                @endforeach
-                                @foreach ($favorite->shop->genres as $genre)
-                                    <p class="shop__tag-info">#{{ $genre->name }}</p>
-                                @endforeach
-                            </div>
-                            <div class="shop__button">
-                                <a href="{{ route('shop.detail', ['shop' => $favorite->shop->id]) }}" class="shop__button-detail">詳しくみる</a>
-                                <div class="stage">
-                                    <button class="heart heart-active" data-shop-id="{{ $favorite->shop->id }}" aria-label="お気に入り" type="button"></button>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                @endforeach
-            @else
-                <div class="favorite__placeholder">お気に入りの店舗はありません</div>
-            @endif
+      <p class="user__name">{{ Auth::user()->name }}さん</p>
+      <p class="favorite__title">お気に入り店舗</p>
+      <div class="shop__wrap">
+        @if (isset($favoriteShops) && count($favoriteShops) > 0)
+        @foreach ($favoriteShops as $favorite)
+        <div class="shop__content">
+          @foreach ($favorite->shop->images as $image)
+          <img src="{{ $image->shop_image_url }}" alt="{{ $favorite->shop->name }}" class="shop__image">
+          @endforeach
+          <div class="shop__item">
+            <h2 class="shop__title">{{ $favorite->shop->name }}</h2>
+            <div class="shop__tag">
+              @foreach ($favorite->shop->areas as $area)
+              <p class="shop__tag-info">#{{ $area->name }}</p>
+              @endforeach
+              @foreach ($favorite->shop->genres as $genre)
+              <p class="shop__tag-info">#{{ $genre->name }}</p>
+              @endforeach
+            </div>
+            <div class="shop__button">
+              <a href="{{ route('shop.detail', ['shop' => $favorite->shop->id]) }}" class="shop__button-detail">詳しくみる</a>
+              <div class="stage">
+                <button class="heart heart-active" data-shop-id="{{ $favorite->shop->id }}" aria-label="お気に入り" type="button"></button>
+              </div>
+            </div>
+          </div>
         </div>
+        @endforeach
+        @else
+        <div class="favorite__placeholder">お気に入りの店舗はありません</div>
+        @endif
+      </div>
     </div>
-</div>
-@endsection
+  </div>
+  @endsection
 
-@section('script')
-<script src="{{ asset('js/toggleFavorite.js') }}"></script>
-@endsection
+  @section('script')
+  <script src="{{ asset('js/toggleFavorite.js') }}"></script>
+  @endsection
--- src/resources/views/partials/bookings.blade.php
+++ src/resources/views/partials/bookings.blade.php
@@ -5,6 +5,10 @@
       </span>
       <a href="{{ route('bookings.show', ['booking' => $booking->id]) }}" class="booking__header__title">
     </a>
+
+    {{-- 予約変更ボタンの設定 --}}
+    <button type="button" class="form__button form__button--change" data-id="{{ $booking->id }}" onclick="openModal({!! $booking->id !!)"><img src="{{ asset('images/pencil.png') }}" alt="変更" class="form__button-img white-image">予約変更</button>
+    
     <div class="booking__header-button">
       <form id="cancelForm{{ $booking->id }}" method="POST" action="{{ route('bookings.cancel', ['booking' => $booking->id]) }}" class="header__form">
         @csrf
@@ -28,6 +32,7 @@
     <p><span class="number-label">Number</span> {{ $booking->number_of_people }}人</p>
   </div>
 </div>
+
 
 @section('script')
 <script src="{{ asset('js/cancelBooking.js') }}"></script>
