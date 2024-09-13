366abbd8a74a65963fa4b2a38fc76d2a09c267ab
fukumamire <rinka_karyou@hotmail.com>
2024-09-13 09:30:50 +0900

    管理者登録　controllerの修正　

--- src/app/Http/Controllers/Auth/AdminRegisterController.php
+++ src/app/Http/Controllers/Auth/AdminRegisterController.php
@@ -6,10 +6,10 @@ use App\Models\User;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-
-use Spatie\Permission\Models\Role;
-
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+use Spatie\Permission\Models\Role;
 
 class AdminRegisterController extends Controller
 {
@@ -29,26 +29,32 @@ class AdminRegisterController extends Controller
     if (!method_exists($this, 'store')) {
       abort(500, 'store method does not exist.');
     }
-
-    $validatedData = $request->validate([
+    $validator = Validator::make($request->all(), [
       'name' => 'required|string|max:255',
       'email' => 'required|string|email|max:255|unique:users',
       'password' => 'required|string|min:8|confirmed',
+      'password_confirmation' => 'required|string|min:8',
     ]);
 
-    // パスワードをハッシュ化
-    $validatedData['password'] = Hash::make($validatedData['password']);
+    if ($validator->fails()) {
+      return redirect()->back()
+        ->withErrors($validator)
+        ->withInput();
+    }
 
-    $user = User::create($validatedData);
+    try {
+      DB::transaction(function () use ($request) {
+        $user = app(\App\Actions\Fortify\CreateNewUser::class)->create(array_merge($request->all(), ['guard' => 'admin']));
 
-    // super-admin ロールを取得または作成
-    $superAdminRole = Role::where('name', 'super-admin')->firstOrCreate(['name' => 'super-admin']);
+        Auth::guard('admin')->login($user);
+      });
 
-    // ユーザーに super-admin ロールを割り当て
-    $user->assignRole($superAdminRole);
-
-    Auth::guard('admin')->login($user);
-
-    return redirect()->route('admin.index')->with('success', '新しい管理者を登録しました。'); // 管理者用のホームページへリダイレクト
+      return redirect()->route('admin.index')
+      ->with('success', '新しい管理者を登録しました。');
+    } catch (\Exception $e) {
+      return redirect()->back()
+        ->withInput()
+        ->withErrors(['error' => '登録中にエラーが発生しました。再度試してください。']);
+    } 
   }
 }
